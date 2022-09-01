49baadcf1dfc474219962a47aef13b70d80159e7
Vitalii Iashchuk <iashchuk@hotmail.com>
2022-09-01 23:55:31 +0300

    6kyu: multiples-of-3-or-5

---
+++ src/6kyu/multiples-of-3-or-5.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/514b92a657cdc65150000006
+
+const solution = (number) => {
+  let result = 0;
+
+  for (let i = 1; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      result += i;
+    }
+  }
+
+  return result;
+};
